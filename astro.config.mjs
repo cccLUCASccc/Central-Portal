901bd84788c56662345a5656257020e87d574426
Cloes Lucas <lucascloes@gmail.com>
2026-04-17 22:54:23 +0200

    login

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 import svelte from '@astrojs/svelte';
 
+import node from "@astrojs/node";
+
+import clerk from "@clerk/astro";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [svelte()]
+  integrations: [svelte(), clerk()],
+  adapter: node({ mode: "standalone" }),
+  output: "server"
 });
