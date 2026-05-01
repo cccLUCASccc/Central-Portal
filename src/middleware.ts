af8a1c1a4f1af9de1ce1b378b2879bd95eebc5fa
LucasC <lucascloes@gmail.com>
2026-05-01 11:56:35 +0200

    delete acces restrein page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,14 @@
-import { clerkMiddleware } from "@clerk/astro/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
-export const onRequest = clerkMiddleware();
+// On définit les routes qui ne nécessitent pas d'authentification
+// Ici, on veut que TOUTES les pages soient protégées.
+const isPublicRoute = createRouteMatcher([]);
+
+export const onRequest = clerkMiddleware((auth, context) => {
+  const { userId } = auth();
+
+  // Si l'utilisateur n'est pas connecté et que la route n'est pas publique
+  if (!userId && !isPublicRoute(context.request)) {
+    return auth().redirectToSignIn();
+  }
+});
